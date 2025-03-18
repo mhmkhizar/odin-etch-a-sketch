f4321c5706c40212b5cc8497158ecf6d1f1c84a6
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-18 09:43:02 +0500

    Refactor grid square generation and add mouseover color effect

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const gridContainer = document.querySelector(".grid-container");
-const squares = generateGridSquares(16);
+const gridSquares = generateGridSquares(16);
 
-gridContainer.append(...squares);
+gridContainer.append(...gridSquares);
+
+gridContainer.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("square")) {
+    e.target.classList.add("color");
+  }
+});
 
 function generateGridSquares(size) {
   const squaresQuantity = size * size;
+  const squareSize = 400 / size;
   const squares = [];
 
   for (let i = 0; i < squaresQuantity; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
     squares.push(square);
   }
 
