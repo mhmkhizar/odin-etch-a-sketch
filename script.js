9a8fa136aace79565be068e3bb811b0da4c73350
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-19 07:27:37 +0500

    Implement custom color picker modes and enhance grid square interactions

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 const gridContainer = document.querySelector(".grid-container");
-const colorInput = document.querySelector("#color-picker");
+const colorInput = document.querySelector("#colorPicker");
 const customColorPicker = document.querySelector(".custom-color-picker");
+const buttons = document.querySelector(".buttons");
+let selection = document.querySelector("#colorMode");
 
 const gridSquares = generateGridSquares(16);
 gridContainer.append(...gridSquares);
 
+let userSelectedColor = colorInput.value;
+
 colorInput.addEventListener("input", (e) => {
-  customColorPicker.style.backgroundColor = e.target.value;
+  customColorPicker.style.backgroundColor = colorInput.value;
+  userSelectedColor = e.target.value;
+});
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const previousSelection = document.querySelector(".selected");
+    const currentSelection = e.target;
+
+    if (currentSelection.id === "Clear") {
+      gridSquares.forEach((square) => (square.style.backgroundColor = "#eee"));
+    } else {
+      previousSelection.classList.remove("selected");
+      currentSelection.classList.add("selected");
+      selection = currentSelection;
+    }
+  }
 });
 
 gridContainer.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("square")) {
-    e.target.style.backgroundColor = colorInput.value;
-    e.target.style.boxShadow = `0 0 0 0.05px ${colorInput.value}`;
+    switch (selection.id) {
+      case "colorMode":
+        e.target.style.backgroundColor = userSelectedColor;
+        e.target.style.boxShadow = `0 0 0 0.1px ${userSelectedColor}`;
+        break;
+      case "rainbowMode":
+        e.target.style.backgroundColor = getRandomColor();
+        e.target.style.boxShadow = `0 0 0 0.1px ${getRandomColor()}`;
+        break;
+      case "Eraser":
+        e.target.style.backgroundColor = "#eee";
+        e.target.style.boxShadow = "0 0 0 0.1px #222";
+        break;
+      default:
+        break;
+    }
   }
 });
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 function generateGridSquares(size) {
   const squaresQuantity = size * size;
